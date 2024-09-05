824847714e3ca847f6f69714b463de4208505f66
Vakarva <151787968+Vakarva@users.noreply.github.com>
2024-09-04 18:13:42 -0700

    Project initialized via Vite scaffolding

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+
+    return (
+        <main>
+            <h1>Hello world!</h1>
+        </main>
+    )
+}
